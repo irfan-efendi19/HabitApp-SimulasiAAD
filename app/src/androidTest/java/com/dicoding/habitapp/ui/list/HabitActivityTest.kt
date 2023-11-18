f6b9533e588791bf6a9f81030907e5130d9bfbfc
Mochammad Irfan Efendi <m.irfan.efendi19@gmail.com>
2023-11-18 22:00:14 +0700

    Add Unit Test

--- app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
@@ -1,6 +1,42 @@
 package com.dicoding.habitapp.ui.list
 
-//TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
-class HabitActivityTest {
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.add.AddHabitActivity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
+//TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class HabitActivityTest {
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(HabitListActivity::class.java)
+
+    @Rule
+    @JvmField
+    val taskActivityRule = IntentsTestRule(HabitListActivity::class.java)
+
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(HabitListActivity::class.java)
+    }
+
+    @Test
+    fun openAddHabit() {
+        Espresso.onView(withId(R.id.fab)).perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(AddHabitActivity::class.java.name))
+    }
 }
